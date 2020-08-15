36297daac618850e3d744ae45154a824bba4dd70
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-08-15 05:38:55 -0400

    Finish SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,78 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Deu');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <View>
+              <ForgotPassword
+                onPress={() => {
+                  console.log('Deu');
+                }}
+              >
+                <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+              </ForgotPassword>
+            </View>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton
+        onPress={() => {
+          console.log('Deu');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
